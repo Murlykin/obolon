8f19cf7deb120191d11ee82f14dd31c4da969cc8
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2024-06-27 19:15:18 +0300

    font-size

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -117,7 +117,7 @@ export const HeaderText = styled.p`
   text-shadow: 5px 5px 3px rgba(0,0,0,0.3);
         @media (max-width: 768px){
         line-height: 1.2;
-        font-size: 20px;
+        font-size: 14px;
         padding-left: 120px;
         flex-direction: column;
         }
